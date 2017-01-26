29c4ed8dafff73057e7ae1e838455e5d31b3a132
Unknown <sabrimtir9@gmail.com>
2017-01-26 02:51:42 +0100

    Init upgrade files

---
+++ inc/function.inc.php
@@ -0,0 +1,16 @@
+<?php
+//Fonction d'exécution de requêtes 
+function execute_requete($req)
+{
+  global $mysqli;
+  //global $bdd;
+
+  if(isset($_POST['db']))
+  {
+  $mysqli->select_db($_POST['db']);
+  }
+
+  $resultat = $mysqli->query($req);
+  
+  return $resultat; // on retourne le résultat de la requete
+}
